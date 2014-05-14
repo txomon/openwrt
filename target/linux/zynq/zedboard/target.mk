1086097d8e9476f6b9797dd3ede8384887f38992
Javier Domingo Cansino <javierdo1@gmail.com>
2014-05-14 16:23:31 +0200

    Adding zynq board type, with zedboard subtype. Kernel patch taken from the diff between v3.13 and xilinx/master

---
+++ target/linux/zynq/zedboard/target.mk
@@ -0,0 +1,9 @@
+ARCH:=arm
+SUBTARGET:=zedboard
+BOARDNAME:=Zedboard <zedboard.org>
+DEVICE_TYPE:=developerboard
+
+define Target/Description
+	Build firmware images for Zedboard board, based on Xilinx's
+	zynq board.
+endef
